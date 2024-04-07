e5312eb3748f254f1c2375a6660a739579934e89
Utsav <111552902+Utsav-06@users.noreply.github.com>
2024-04-07 15:22:33 +0530

    Added show password 'eye' and its logic in both login and sign up page

---
+++ ClassHub/classUser/static/Js/SignUpPage.js
@@ -0,0 +1,14 @@
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById("password");
+    var toggleIcon = document.querySelector(".toggle-password i");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        toggleIcon.classList.remove("bi-eye");
+        toggleIcon.classList.add("bi-eye-slash");
+    } else {
+        passwordField.type = "password";
+        toggleIcon.classList.remove("bi-eye-slash");
+        toggleIcon.classList.add("bi-eye");
+    }
+}
